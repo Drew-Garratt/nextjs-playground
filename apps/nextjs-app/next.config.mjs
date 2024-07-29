9773e3f0a1346fe4278ed9c9e511515f093f823f
Drew Garratt <drew_garratt@mckinsey.com>
2024-07-29 12:05:36 +0100

    fix: removed duplicate sentry config

--- apps/nextjs-app/next.config.mjs
+++ apps/nextjs-app/next.config.mjs
@@ -2,6 +2,7 @@ import {withSentryConfig} from "@sentry/nextjs";
 import path from "node:path";
 import url from "node:url";
 import withNextIntl from 'next-intl/plugin';
+import pc from 'picocolors';
 
 import {env} from "./src/env.mjs";
 
@@ -100,42 +101,6 @@ const nextConfig = {
   },
 };
 
-export default withSentryConfig(nextWithIntl, {
-// For all available options, see:
-// https://github.com/getsentry/sentry-webpack-plugin#options
-
-org: "drew-garratt",
-project: "javascript-nextjs",
-
-// Only print logs for uploading source maps in CI
-silent: !env.NEXT_BUILD_ENV_CI,
-
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
-
-// Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-// tunnelRoute: "/monitoring",
-
-// Hides source maps from generated client bundles
-hideSourceMaps: true,
-
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
-
-// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
-});
-
-
 let config = nextConfig;
 
 // Next.js Configuration with `next.intl` enabled
@@ -160,6 +125,22 @@ if (
       // For all available options, see:
       // https://github.com/getsentry/sentry-webpack-plugin#options.
       // silent: isProd, // Suppresses all logs
+
+      // Upload a larger set of source maps for prettier stack traces (increases build time)
+      widenClientFileUpload: true,
+
+      // Hides source maps from generated client bundles
+      hideSourceMaps: true,
+
+      // Automatically tree-shake Sentry logger statements to reduce bundle size
+      disableLogger: true,
+
+      // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+      // See the following for more information:
+      // https://docs.sentry.io/product/crons/
+      // https://vercel.com/docs/cron-jobs
+      automaticVercelMonitors: true,
+
       silent: env.NEXT_BUILD_ENV_SENTRY_DEBUG === false,
     });
     console.log(`- ${pc.green('info')} Sentry enabled for this build`);
